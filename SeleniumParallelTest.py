15704c7d83da1524c0b8e66059c1263955d34a2a
y24 <vintage1022@gmail.com>
2025-01-26 17:54:43 +0900

    Logger

--- SeleniumParallelTest.py
+++ SeleniumParallelTest.py
@@ -1,35 +1,36 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from modules import WebDriver as WD
+from modules import Logger
 
 # Chromeをリモートデバッグモードで起動しておく
 # cd C:\Program Files\Google\Chrome\Application
 # chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chrome-debug"
 
+# Logger
+logger = Logger.get_logger(__name__, console=True, file=False, trace_line=False)
+
 try:
     # WebDriverセットアップ
     driver = WD.setup_driver(headless=False, remote_debug=True, page_load_strategy=True)
+    logger.info("Start WebDriver")
 
     # 検索クエリ
     queries = ["bbb", "ccc", "ddd", "fff"]
 
     # 全タブを取得
     all_tabs = driver.window_handles
-    print(f"Handles: {all_tabs}")
+    logger.debug(f"Tab Handles: {all_tabs}")
 
     # 各タブを操作(検索)
     for index, query in enumerate(queries):
         # タブを切り替え
         target_tab = all_tabs[index]
         driver.switch_to.window(target_tab)
-
-        print(f"index: {index} / tab_handle: {target_tab}")
-        print(f"query: {queries[index]}")
+        logger.debug(f"Tab {index}: {target_tab}")
 
         # Googleに移動
         driver.get("https://www.google.co.jp")
@@ -41,10 +42,11 @@ try:
         query = queries[index]
         search_box.send_keys(query)
         search_box.send_keys(Keys.RETURN)
+        logger.debug(f"Search: {query}")
 
         # 検索結果の一番上のリンクを出力
         search_result = WD.get_text_by_xpath(driver, '//h3')
-        print(f"Result: {search_result}")
+        logger.debug(f"Result: {search_result}")
 
     # 各タブを操作(リンククリック)
     for tab_handle in all_tabs:
@@ -56,4 +58,4 @@ try:
         link.click()
 
 except Exception as e:
-    print(f"Error: {e}")
+    logger.error(e)
---
+++ modules/Logger.py
@@ -0,0 +1,44 @@
+from logging import getLogger, StreamHandler, FileHandler, Formatter, ERROR, WARNING, INFO, DEBUG
+
+# カスタムフォーマッター
+class CustomFormatter(Formatter):
+    # 色設定
+    COLOR_MAP = {
+        ERROR: "\033[91m",  # 赤
+        WARNING: "\033[93m",  # 黄
+        INFO: "\033[92m",  # 緑
+        # DEBUG: "\033[94m",  # 青
+        "RESET": "\033[0m",  # リセット
+    }
+
+    def format(self, record):
+        color = self.COLOR_MAP.get(record.levelno, self.COLOR_MAP["RESET"])
+        reset = self.COLOR_MAP["RESET"]
+        message = super().format(record)
+        return f"{color}{message}{reset}"
+
+def get_logger(name:str, console=True, file=False, trace_line=False):
+    """Loggerを作成する"""
+    logger = getLogger(name)
+    logger.setLevel(DEBUG)
+
+    format = '%(asctime)s - %(levelname)s : %(name)s : %(message)s'
+    if trace_line:
+        format += ' (%(filename)s:%(lineno)d)'
+    formatter = CustomFormatter(format)
+
+    # コンソール出力
+    if console:
+        s_handler = StreamHandler()
+        s_handler.setLevel(DEBUG)
+        s_handler.setFormatter(formatter)
+        logger.addHandler(s_handler)
+
+    # ファイル出力
+    if file:
+        f_handler = FileHandler("./app.log")
+        f_handler.setLevel(DEBUG)
+        f_handler.setFormatter(formatter)
+        logger.addHandler(f_handler)
+    
+    return logger
